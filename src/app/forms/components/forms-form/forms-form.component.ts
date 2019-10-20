6e666f4f5f766c464c174bf8e0b3f526ee8f3dbc
kareem arafa <k4ho@live.com>
2019-10-20 18:37:38 +0300

    add translation keys

--- src/app/forms/components/forms-form/forms-form.component.ts
+++ src/app/forms/components/forms-form/forms-form.component.ts
@@ -19,35 +19,6 @@ export class FormsFormComponent extends CoreFormComponent implements OnInit, OnD
 		super(service, fb, activatedRoute, utilities);
 	}
 
-	initLists() {
-		this.lists = [
-			{
-				[this.cid + '/steps']: 1
-			}
-		];
-	}
-
-	// refactorItem(item: any): any {
-	//   super.refactorItem(item);
-	//   /**
-	//    * Set value of selected items
-	//    */
-	//   const selectedUsers = [];
-	//   for (const user of item.users) {
-	//     selectedUsers.push(user.user_id);
-	//   }
-	//   this.form.controls.branch_managers.setValue(selectedUsers); // Final Return
-	//   return item;
-	// }
-
-	get lists() {
-		return this._lists;
-	}
-
-	set lists(value: any) {
-		this._lists = value;
-	}
-
 	ngOnInit() {
 		super.ngOnInit();
 	}
